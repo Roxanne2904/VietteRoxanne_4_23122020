6319c259a28b06740ca6d7bf80c23522afe7eba3
Roxanne <roxanne-viette@hotmail.fr>
2021-05-03 21:23:24 +0200

    réa du msg de validation + condition inputs radios terminées + debut responsive vérifs

--- README.md
+++ README.md
@@ -13,3 +13,19 @@
 26/04/2021
 
 //conditions des inputs radios, en cours de réa.
+
+03/05/2021
+
+//Très grosse progression :
+
+// 1.Transformation de la nodelist en array;
+// 2.Création de L'array contenant tous les input (array[0]= "input txt et checkbox" et array[1]="input radios");
+// 3.Validation du .checked pour les inputs radios; 
+// 4.Réorganisation des conditions en utilisant le arrayInputsAll, avec une boucle for;
+// 5.Utilisation d'un every() et d'un some() en tant que conditon, pour valider;
+     l'ensemble du formulaire et y injecter le msg de validation; 
+// 6.Début de la vérification du responsive;
+
+//Reste à faire :
+// 1.Mettre le bouton à l'identique que dans le form (ajouter un newelt au modal-body ?);
+// 2.Mettre en place le responsive du form en version mobile);
--- modal.js
+++ modal.js
@@ -8,98 +8,187 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalBg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 //[2 btn], il y en a un pour le responsive et l'autre pour la vers.desktop.
 const xBtn = document.querySelector(".close");
 
 function modalEvent() {
-  modalbg.style.display = "block";
+  modalBg.style.display = "block";
 }
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", modalEvent));
 
 //launch modal event close
 xBtn.addEventListener("click", (event) => {
-  modalbg.style.display = "none";
+  modalBg.style.display = "none";
 });
+
+//*******************************************/
+// DOM ELEMENTS INPUTS
 //*******************************************/
 const first = document.getElementById("first");
 const last = document.getElementById("last");
 const email = document.getElementById("email");
 const birthdate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
-//********/
-const arrayInputsTxt = [first, last, email, birthdate, quantity];
+const checkbox1 = document.querySelector("#checkbox1");
+const radios = document.querySelectorAll(".checkbox-input[type='radio']");
+console.log(radios); //NODELIST DES INPUTS RADIOS
+let radiosArray = Array.from(radios);
+console.log(radiosArray); //NODELIST DEVIENT UN ARRAY
+
 //*******************************************/
-//Validation du formulaire
+//ARRAY DE TOUS LES INPUTS
+//*******************************************/
+const arrayInputsAll = [
+  [first, last, email, birthdate, quantity, checkbox1],
+  radiosArray,
+];
+console.log(arrayInputsAll);
+
+const arrayInputsTxt = [first, last, email, birthdate, quantity]; //ARRAY DES INPUTS TXT
+
+//*******************************************/
+//CHANGEMENT DU MSG, LORS DU CHANGEMENT DU CONTENUE DE L'INPUT
+//*******************************************/
+arrayInputsTxt.forEach((elmt) => {
+  elmt.addEventListener("change", () => {
+    elmt.parentElement.dataset.errorVisible = "false";
+  });
+});
+
+//*******************************************/
+//VALIDATION DU FORMULAIRE
 //*******************************************/
 const validate = (event) => {
-  arrayInputsTxt.forEach((items) => {
-    const isValid = items.validity.valid;
-    console.log(items, isValid);
-    //
+  event.preventDefault();
+
+  for (i = 0; i < 6; i++) {
+    const isValid = arrayInputsAll[0][i].validity.valid;
     if (!isValid) {
-      event.preventDefault();
-      console.log(items.parentElement);
-      items.parentElement.dataset.errorVisible = "true";
-      arrayInputsTxt[0].parentElement.dataset.error =
+      arrayInputsAll[0][i].parentElement.dataset.errorVisible = "true";
+      arrayInputsAll[0][0].parentElement.dataset.error =
         "Le prénom doit être composé de lettres uniquement et commencer par un majuscule";
-      arrayInputsTxt[1].parentElement.dataset.error =
+      arrayInputsAll[0][1].parentElement.dataset.error =
         "Le nom doit être entièrement en majuscule";
-      arrayInputsTxt[2].parentElement.dataset.error =
+      arrayInputsAll[0][2].parentElement.dataset.error =
         "Il faut rentrer une adresse e-mail valide";
-      arrayInputsTxt[3].parentElement.dataset.error =
+      arrayInputsAll[0][3].parentElement.dataset.error =
         "Veuillez indiquer votre date de naissance";
-      arrayInputsTxt[arrayInputsTxt.length - 1].parentElement.dataset.error =
+      arrayInputsAll[0][4].parentElement.dataset.error =
         "Veuillez rentrer un chiffre ou un nombre entre 0 et 99";
     } else {
-      items.parentElement.dataset.errorVisible = "false";
-      items.parentElement.dataset.error = "";
+      arrayInputsAll[0][i].parentElement.dataset.errorVisible = "false";
+      arrayInputsAll[0][i].parentElement.dataset.error = "";
     }
-  });
-  arrayInputsTxt.forEach((elmt) => {
-    elmt.addEventListener("change", () => {
-      event.stopPropagation();
-      elmt.parentElement.dataset.errorVisible = "false";
-    });
-  });
-
-  //CHECKBOX RADIOS ....en cours de réa
-  //******/
-  //const radioChecked = document.querySelector(
-  //".checkbox-input[type='radio']:checked"
-  //);
-  //*****/
-  const radios = document.querySelectorAll(".checkbox-input[type='radio']");
-  console.log(radios);
-
-  let checkboxchecked = false;
-  for (i = 0; i < radios.length; i++) {
-    if (radios[i].checked === true) {
+    //*******************************************/
+    //CHECKBOX RADIOS
+    //*******************************************/
+    let checkboxRadiochecked = false;
+    arrayInputsAll[1][i].parentElement.dataset.errorVisible = "false";
+    if (arrayInputsAll[1][i].checked === true) {
+      checkboxRadiochecked = true;
+      break;
+    }
+    if (checkboxRadiochecked === false) {
+      arrayInputsAll[1][0].parentElement.dataset.errorVisible = "true";
+      arrayInputsAll[1][0].parentElement.dataset.error = "nope";
+    }
+    //*******************************************/
+    //CHECKBOX CHECK
+    //*******************************************/
+    arrayInputsAll[0][5].parentElement.dataset.errorVisible = "false";
+    let checkboxchecked = false;
+    if (arrayInputsAll[0][5].checked === true) {
       checkboxchecked = true;
-      return true;
-    } else if (checkboxchecked === false) {
-      radios[i].parentElement.dataset.errorVisible = "true";
-      radios[i].parentElement.dataset.error = " nope";
-      return false;
+    } else {
+      arrayInputsAll[0][5].parentElement.dataset.error =
+        "Vous devez accepter les conditions d'utilisations";
+      arrayInputsAll[0][5].parentElement.dataset.errorVisible = "true";
     }
   }
-  //EVERY()
-  //const isItChecked = (currentValue) => currentValue.checked === true;
-  //console.log(radios.every(isItChecked));
-  //******/
+  //*******************************************/
+  //CHECKBOX RADIOS
+  //*******************************************/
+  //const radios = document.querySelectorAll(".checkbox-input[type='radio']");
+  //console.log(radios);
 
+  //let checkboxRadiochecked = false;
+  //radios[0].parentElement.dataset.errorVisible = "false";
+  //for (i = 0; i < radios.length; i++) {
+  // if (radios[i].checked === true) {
+  // checkboxRadiochecked = true;
+  //break;
+  //}
+  //}
+  //if (checkboxRadiochecked === false) {
+  //radios[0].parentElement.dataset.errorVisible = "true";
+  //radios[0].parentElement.dataset.error = "nope";
+  //}
+
+  //*******************************************/
+  //SOME();
+  //*******************************************/
+  //let radiosArray = Array.from(radios);
+  //console.log(radiosArray);
+  //const isItChecked = (currentValue) => {
+  //console.log("isItChecked");
+  //console.log({ value: currentValue.value, checked: currentValue.checked });
+  //return currentValue.checked === true;
+  //};
+  //console.log(radiosArray.some(isItChecked));
+
+  //*******************************************/
   //CHECKBOX CHECK
-  const checkbox1 = document.querySelector("#checkbox1");
-  console.log(checkbox1);
-  if (!checkbox1.checked) {
-    event.preventDefault();
-    checkbox1.parentElement.dataset.error =
-      "Vous devez accepter les conditions d'utilisations";
-    checkbox1.parentElement.dataset.errorVisible = "true";
-  } else {
-    checkbox1.parentElement.dataset.errorVisible = "false";
+  //*******************************************/
+
+  //checkbox1.parentElement.dataset.errorVisible = "false";
+  //let checkboxchecked = false;
+  //if (checkbox1.checked === true) {
+  //checkboxchecked = true;
+  //} else {
+  //checkbox1.parentElement.dataset.error =
+  //"Vous devez accepter les conditions d'utilisations";
+  //checkbox1.parentElement.dataset.errorVisible = "true";
+  //}
+  //alerte = "hello";
+  //*******************************************/
+  //MESSAGE DE VALIDATION
+  //*******************************************/
+  //const modalBody = document.querySelector(".modal-body");
+  //console.log(modalBody);
+  //modalBody.style.opacity = "0";
+  const allIsOk = (currentValue) => {
+    console.log("allIsOk");
+    console.log({
+      value: currentValue.value,
+      checked: currentValue.checked,
+      formValidate: currentValue.validity.valid,
+    });
+    return (
+      currentValue.checked === true || currentValue.validity.valid === true
+    );
+  };
+  console.log(arrayInputsAll[0].every(allIsOk));
+  console.log(arrayInputsAll[1].some(allIsOk));
+  if (
+    arrayInputsAll[0].every(allIsOk) === true &&
+    arrayInputsAll[1].some(allIsOk) === true
+  ) {
+    const byeInputs = document.querySelectorAll(".formData");
+    const byeTxtLabel = document.querySelector(".text-label");
+    const content = document.querySelector(".content");
+    let newElt = document.createElement("div");
+    byeInputs.forEach((item) => {
+      item.style.opacity = "0";
+      return;
+    });
+    byeTxtLabel.style.opacity = "0";
+    content.appendChild(newElt);
+    newElt.innerHTML =
+      "<h3>Merci ! :D</br>Votre Réservation à bien été enregistré</h3>";
+    newElt.classList.add("msgValide");
+    document.querySelector(".btn-submit").setAttribute("disabled", "");
   }
-  return true;
 };
